3b207b913c3e1624c3e3b9f679af1bc26372c9bd
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2022-05-18 00:43:12 +0700

    add WebConfig

--- src/main/java/com/tirmizee/configuration/WebConfig.java
+++ src/main/java/com/tirmizee/configuration/WebConfig.java
@@ -1,2 +1,28 @@
-package com.tirmizee.configuration;public class WebConfig {
+package com.tirmizee.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
+
+    @Bean
+    public ObjectMapper jacksonMapper() {
+        return new ObjectMapper();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*");
+    }
+
 }
