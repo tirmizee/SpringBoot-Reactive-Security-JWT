2b899a2c8fd841240f018e1e92d70c0d8201ef34
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2022-05-18 00:47:09 +0700

    add DatabaseConfig

--- src/main/java/com/tirmizee/configuration/DatabaseConfig.java
+++ src/main/java/com/tirmizee/configuration/DatabaseConfig.java
@@ -1,2 +1,35 @@
-package com.tirmizee.configuration;public class DatabaseConfig {
+package com.tirmizee.configuration;
+
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+@Configuration
+public class DatabaseConfig {
+
+    public static final String SCRIPT_SCHEMA = "schema.sql";
+    public static final String SCRIPT_DATA = "data.sql";
+
+    @Bean
+    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+
+        ClassPathResource schemaResource = new ClassPathResource(SCRIPT_SCHEMA);
+        ClassPathResource dataResource = new ClassPathResource(SCRIPT_DATA);
+
+        CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
+        populator.addPopulators(new ResourceDatabasePopulator(schemaResource));
+        populator.addPopulators(new ResourceDatabasePopulator(dataResource));
+
+        ConnectionFactoryInitializer factoryInitializer = new ConnectionFactoryInitializer();
+        factoryInitializer.setConnectionFactory(connectionFactory);
+        factoryInitializer.setDatabasePopulator(populator);
+        factoryInitializer.setEnabled(false);
+
+        return factoryInitializer;
+    }
+
 }
