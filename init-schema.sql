2c42c1fc10aaea94c4c92724bd81da85b42fbe1d
Pratya Yeekhaday <outsource1842@setth.onmicrosoft.com>
2024-01-02 00:12:38 +0700

    revise docker compose

--- init-schema.sql
+++ init-schema.sql
@@ -1,6 +1,6 @@
-CREATE SCHEMA IF NOT EXISTS schema_sit;
+CREATE SCHEMA schema_sit;
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE schema_sit.users (
 	id serial PRIMARY KEY,
 	username VARCHAR(50) UNIQUE NOT NULL,
 	password VARCHAR(255) NOT NULL,
--- src/main/resources/database/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO users (username, password, email, enabled, created_date, last_login) VALUES('tirmizee', '{bcrypt}$2a$10$kvHzEVdElMw.7IcTjEAZf.r/x6tANA0WxhD0cl6RCN7RDzsUdvxCe', 'tirmizee@gmail.com', TRUE, CURRENT_TIMESTAMP, null);
