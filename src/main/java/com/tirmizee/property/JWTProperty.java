989861831bacdaca9d8b6591523d7c331fc06b74
Pratya Yeekhaday <outsource1842@setth.onmicrosoft.com>
2024-01-03 12:07:16 +0700

    refactor code

--- src/main/java/com/tirmizee/property/JWTProperty.java
+++ src/main/java/com/tirmizee/property/JWTProperty.java
@@ -1,8 +1,13 @@
 package com.tirmizee.property;
 
 import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @Data
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
 public class JWTProperty {
-
+    private String secret;
+    private long expiration;
 }
--- src/main/java/com/tirmizee/security/JWTProvider.java
+++ src/main/java/com/tirmizee/security/JWTProvider.java
@@ -1,10 +1,12 @@
 package com.tirmizee.security;
 
 import com.tirmizee.exception.data.JWTSignatureException;
+import com.tirmizee.property.JWTProperty;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,11 @@ import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.Date;
 
+@AllArgsConstructor
 @Component
 public class JWTProvider {
 
-    private String key = "11111111111111111111111111111111";
+    private JWTProperty jwtProperty;
 
     public String generateToken(Authentication authentication) {
 
@@ -24,21 +27,22 @@ public class JWTProvider {
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
 
         Date now = new Date();
-        Date validity = new Date(now.getTime() + 3600000);
+        Date validity = new Date(now.getTime() + jwtProperty.getExpiration());
         Claims claims = Jwts.claims().setSubject(username);
+        claims.put("ip", "");
 
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(validity)
-                .signWith(Keys.hmacShaKeyFor(key.getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS256)
+                .signWith(Keys.hmacShaKeyFor(jwtProperty.getSecret().getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public Claims getClaims(String token) {
         try {
             return Jwts.parserBuilder()
-                    .setSigningKey(key.getBytes(StandardCharsets.UTF_8))
+                    .setSigningKey(jwtProperty.getSecret().getBytes(StandardCharsets.UTF_8))
                     .build()
                     .parseClaimsJws(token)
                     .getBody();
